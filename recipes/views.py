876e8829f3531b83872895fe9fd9694b7b2886b3
Victor Abreu <aalevictor@gmail.com>
2022-02-27 16:16:20 -0300

    Views e URLs

--- recipes/views.py
+++ recipes/views.py
@@ -5,11 +5,3 @@ def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Victor Abreu',
     })
-
-
-def sobre(request):
-    return render(request, 'recipes/sobre.html')
-
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
